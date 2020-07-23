1f2fdd84ba27eb9b7bcefa8d4e043ac462b490e6
iceBear67 <icebear67@sfclub.cc>
2020-07-23 11:38:28 +0800

    [ADD] /event

--- src/main/java/pw/illusion/randevent/Main.java
+++ src/main/java/pw/illusion/randevent/Main.java
@@ -1,6 +1,10 @@
 package pw.illusion.randevent;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import pw.illusion.randevent.events.EventStart;
 
 public final class Main extends JavaPlugin {
 
@@ -14,5 +18,14 @@ public final class Main extends JavaPlugin {
         reloadConfig();;
         getLogger().info("Starting!!");
         EventManager.getInstance().getDefaultRunner().runTaskTimerAsynchronously(this,0,getConfig().getInt("tickRate")*20);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(args.length!=2){
+            return false;
+        }
+        Bukkit.getPluginManager().callEvent(new EventStart(args[1]));
+        return true;
     }
 }
